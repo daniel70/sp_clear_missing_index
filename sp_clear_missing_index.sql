de10b84dbd702fae32abf8abc0a079bdab82c5d5
daniel <vdmeulen.daniel@gmail.com>
2020-08-23 04:11:28 +0200

    Work in progress, too late to continue.

--- sp_clear_missing_index.sql
+++ sp_clear_missing_index.sql
@@ -14,8 +14,8 @@ select distinct database_id, object_id, statement
 into #missing_index
 from sys.dm_db_missing_index_details det
 
-alter table #missing_index
-add column_name sysname, type_name VARCHAR(9)
+-- alter table #missing_index
+-- add column_name sysname, type_name VARCHAR(9)
 
 CREATE TABLE #missing_index_data_types (
 	id SMALLINT IDENTITY NOT NULL PRIMARY KEY (id),
@@ -41,46 +41,56 @@ INSERT INTO #missing_index_data_types (type_id, type_name) VALUES
 	,(35, 'CHARACTER') -- text
 	,(99, 'CHARACTER') -- ntext
 
-DECLARE 
-	@database_id int,
+DECLARE @database_id int,
 	@object_id int,
-	@statement varchar(1000), -- TODO: find date type
+	@statement nvarchar(1000), -- TODO: find date type
 	@db_name SYSNAME,
 	@stmt NVARCHAR(1000),
 	@column_name SYSNAME,
-	@type_name VARCHAR(9);
+	@type_name VARCHAR(9),
+	@create_stmt VARCHAR(1000),
+	@drop_stmt VARCHAR(1000);
 
-declare table_cursor scroll cursor for
-select mi.database_id, mi.object_id, mi.statement, column_name, type_name from #missing_index mi
-for update of column_name, type_name;
+declare table_cursor cursor for
+select mi.database_id, mi.object_id, mi.statement from #missing_index mi
+-- for update of column_name, type_name;
 
 open table_cursor;
-fetch next from table_cursor into @database_id, @object_id, @statement, @column_name, @type_name;
+fetch next from table_cursor into @database_id, @object_id, @statement;
 
 	
 while @@FETCH_STATUS = 0 begin
 	set @db_name = QUOTENAME(db_name(@database_id));
 	set @stmt = concat(
-		'select top 1 @column_name = c.name, @type_name = dt.type_name ', @db_name, '.sys.columns c
+		'select top 1 @column_name = c.name, @type_name = dt.type_name from', @db_name, '.sys.columns c
 			join #missing_index_data_types dt on c.system_type_id = dt.type_id
 		 where object_id = @object_id
 		 order by dt.id asc
 		');
 	exec sys.sp_executesql @stmt=@stmt, 
-		@params=N'@object_id int, @name sysname OUTPUT, @type_id int OUTPUT',
+		@params=N'@object_id int, @column_name sysname OUTPUT, @type_name VARCHAR(9) OUTPUT',
 		@object_id=@object_id,
 		@column_name = @column_name OUTPUT,
 		@type_name = @type_name OUTPUT
 	
-	update #missing_index set column_name = @column_name, type_name = @type_name where current of table_cursor;
+	-- update #missing_index set column_name = @column_name, type_name = @type_name where current of table_cursor;
 
-	fetch next from table_cursor into @database_id, @object_id, @statement, @column_name, @type_name;
+	if @type_name = 'NUMBER' begin
+		set @create_stmt = CONCAT('CREATE INDEX missing_index ON ', @statement, '(', @column_name, ') WHERE ', @column_name, ' = 42');
+		print @create_stmt;
+--		exec sp_executesql @create_stmt;
+--		set @drop_stmt = CONCAT('DROP INDEX missing_index ON ', @statement);
+--		exec sp_executesql @drop_stmt;
+	end
+	else begin
+		set @create_stmt = CONCAT('CREATE INDEX missing_index ON ', @statement, '(', @column_name, ') WHERE ', @column_name, ' = ''42''');
+		exec sp_executesql @create_stmt;
+		set @drop_stmt = CONCAT('DROP INDEX missing_index ON ', @statement);
+		exec sp_executesql @drop_stmt;
+	end
+	fetch next from table_cursor into @database_id, @object_id, @statement;
 
 end;
-
---fetch first from table_cursor;
-
-
 close table_cursor
 deallocate table_cursor
 
